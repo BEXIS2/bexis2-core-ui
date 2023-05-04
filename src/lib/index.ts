7eb108758b5f76eadcafe228226c65bf57340cd9
David Schöne <david.blaa@googlemail.com>
2023-05-04 16:40:42 +0200

    setup package stuff
    Co-authored-by: Sven Thiel <sventhiel@users.noreply.github.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,13 +7,9 @@ import FileIcon from './components/File/FileIcon.svelte'
 import FileInfo from './components/File/FileInfo.svelte'
 import FileUploader from './components/File/FileUploader.svelte'
 
-
-
 export {ListView,TableView, FileInfo, FileIcon, FileUploader }
-
 
 export {Api} from './services/Api.js'
 export {host,username,password,setApiConfig} from './stores/apistore.js'
 
 export type {user, FileUploaderModel} from './models/Models.js'
-
