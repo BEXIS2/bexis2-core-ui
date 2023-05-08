5fd2ab164db411a948ef2b80f8263f871d17e835
David Schöne <david.blaa@googlemail.com>
2023-05-08 08:16:41 +0200

    remove test

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,9 +7,8 @@ import FileIcon from './components/File/FileIcon.svelte'
 import FileInfo from './components/File/FileInfo.svelte'
 import FileUploader from './components/File/FileUploader.svelte'
 import Spinner from './components/Spinner/Spinner.svelte'
-import Test from './components/test.svelte'
 
-export {ListView,TableView, FileInfo, FileIcon, FileUploader, Spinner,Test }
+export {ListView,TableView, FileInfo, FileIcon, FileUploader, Spinner }
 
 export {Api} from './services/Api.js'
 export {host,username,password,setApiConfig} from './stores/apistore.js'
