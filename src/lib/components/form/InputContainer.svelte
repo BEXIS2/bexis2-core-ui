07a9e15c0ac124f25e9c5e648808eddcb7bdd02a
David Schöne <david.blaa@googlemail.com>
2023-05-10 09:53:14 +0200

    v0.0.23

---
+++ src/lib/components/form/InputContainer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+ export let label:string = "";
+ export let required:boolean;
+ export let feedback:[];
+ 
+ 
+ </script>
+ 
+ <label class="label">
+  <span>{label} 
+   {#if required} <span class="text-sm text-red-600">*</span> {/if}
+  </span>
+   <slot/>
+   {#if feedback}
+    <span class="text-sm text-error-600">
+      <ul>
+        {#each feedback as message}
+          <li>{message}</li>
+        {/each}
+      </ul>
+    </span>
+   {/if}
+ </label>
+ 
+ 
+ 
