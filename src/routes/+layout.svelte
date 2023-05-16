05965beddc57e8ad0534a3d29d6ad970f372e39f
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-16 10:45:44 +0200

    Add config for highlights.js in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,34 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/all.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	// For the floating UI elements such as column filters
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	// Highlight.js for syntax highlighting in code blocks
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
 	import { AppShell, TableOfContents } from '@skeletonlabs/skeleton';
 	import Navigation from '$docs/Navigation.svelte';
 
+	// Config for the floating UI elements
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+	// Config for the syntax highlighting
+	storeHighlightJs.set(hljs);
 </script>
-
 
 <AppShell
 	slotSidebarLeft="flex-none overflow-x-hidden overflow-y-auto bg-surface-50-900-token lg:w-auto"
-	>
+>
 	<svelte:fragment slot="header">Header</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<Navigation />
 	</svelte:fragment>
-	<svelte:fragment slot="sidebarRight" >
+	<svelte:fragment slot="sidebarRight">
 		<TableOfContents target="#toc-target" />
 	</svelte:fragment>
 	<!-- (pageHeader) -->
