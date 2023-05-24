91fe893459e36b02d41e0f9c9db021fc7c70ec30
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-24 14:43:37 +0200

    Replace CodeBlocks with CodeContainers, add toc

--- src/routes/components/table/+page.svelte
+++ src/routes/components/table/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import '$lib/css/themes/theme-bexis2.css';
 	import { writable } from 'svelte/store';
-	import { CodeBlock, Tab, TabGroup, type PopupSettings, popup } from '@skeletonlabs/skeleton';
+	import { CodeBlock, type PopupSettings, popup } from '@skeletonlabs/skeleton';
 
+	import CodeContainer from '$docs/components/CodeContainer.svelte';
 	import Table from '$lib/components/Table/Table.svelte';
 	import TableOptions from './TableOptions.svelte';
 	import TableFilter from '$lib/components/Table/TableFilter.svelte';
@@ -49,325 +50,227 @@
 		defaultPageSize: 5
 	};
 
-	const popupHoverTableConfig: PopupSettings = {
-		event: 'hover',
-		target: 'popupHoverTableConfig',
+	const popupClickTableConfig: PopupSettings = {
+		event: 'click',
+		target: 'popupClickTableConfig',
 		placement: 'right'
 	};
 
-	const popupHoverColumns: PopupSettings = {
-		event: 'hover',
-		target: 'popupHoverColumns',
+	const popupClickColumns: PopupSettings = {
+		event: 'click',
+		target: 'popupClickColumns',
 		placement: 'right'
 	};
 
-	const popupHoverColumn: PopupSettings = {
-		event: 'hover',
-		target: 'popupHoverColumn',
+	const popupClickColumn: PopupSettings = {
+		event: 'click',
+		target: 'popupClickColumn',
 		placement: 'right'
 	};
-
-	let groupCodeBlocks: number = 0;
-	let usersCodeBlocks: number = 0;
-	let usersNoRolesCodeBlocks: number = 0;
 </script>
 
-<div class="grid gap-5 p-10">
-	<h1 class="font-bold !text-6xl">Table</h1>
-	<div class="grid gap-10 px-10">
-		<div class="grid gap-2">
-			<p class="text-xl">Importing the Table component:</p>
-			<CodeBlock language="ts" code={cb.tableImportCode} />
+<div class="grid gap-20 p-10" id="toc-target">
+	<div class="grid gap-5">
+		<h1 class="font-bold !text-6xl">Table</h1>
+		<h2>Types</h2>
+		<div class="grid gap-2" id="tableConfig">
+			<h3 class="font-semibold relative w-max">
+				{`TableConfig <T>`}
+			</h3>
+			<button class="btn variant-ghost-primary w-min" use:popup={popupClickTableConfig}
+				>Show type details</button
+			>
+			<div class="italic div">Underlined attributes are <strong>required</strong>.</div>
+			<div class="variant-filled-secondary" data-popup="popupClickTableConfig">
+				<CodeBlock language="ts" code={cb.tableConfigTypeCode} />
+			</div>
 		</div>
-		<div class="grid gap-20">
-			<p class="text-xl">
-				A table requires configuration object to connect data to the table and apply customizations
-				correctly. Configuration object is of type <code class="!text-xl">TableConfig</code> and has
-				the following structure:
+
+		<div class="items-center">
+			<div class="flex gap-2 underline">
+				<div class="italic">id:</div>
+				<div class="font-bold">string</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Unique identifier for the table to generate unique IDs for the filters.
 			</p>
-			<div class="grid gap-5">
-				<div class="grid-gap-1" id="tableConfig">
-					<h2 class="font-semibold relative w-max" use:popup={popupHoverTableConfig}>
-						{`TableConfig <T>`}
-					</h2>
-					<h4 class="italic">Underlined attributes are <strong>required</strong>.</h4>
-					<div class="variant-filled-secondary" data-popup="popupHoverTableConfig">
-						<CodeBlock language="ts" code={cb.tableConfigTypeCode} />
-					</div>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2 underline">
-						<h3 class="italic">id:</h3>
-						<h3 class="font-bold">string</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Unique identifier for the table to generate unique IDs for the filters.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2 underline">
-						<h3 class="italic">data:</h3>
-						<h3 class="font-bold">{`Writable <T[]>`}</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						A writable store of the type <code class="!text-xl">T[]</code>. Any changes in the store
-						will be reflected in the table.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">optionsComponent:</h3>
-						<h3 class="font-bold">{`SvelteComponent`}</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Custom Svelte component to apply actions on a specific row. Table will not have an
-						options column if no <code class="!text-xl">optionsComponent</code> was provided.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">pageSizes:</h3>
-						<h3 class="font-bold">{`number[]`}</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						An array of page sizes to be used for the table. By default, page sizes are 5, 10, 15,
-						20.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">defaultPageSize:</h3>
-						<h3 class="font-bold">{`number`}</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Default page size to be used for the table upon rendering. By default, default page size
-						is 10.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">columns:</h3>
-						<h3 class="font-bold">{`Columns`}</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						An object with configuration for specific columns. <code class="!text-xl">Columns</code>
-						object is described below.
-					</p>
-				</div>
-			</div>
-
-			<div class="grid gap-5">
-				<div class="grid-gap-1" id="columns">
-					<h2 class="font-semibold items-center w-max" use:popup={popupHoverColumns}>Columns</h2>
-					<div data-popup="popupHoverColumns">
-						<CodeBlock language="ts" code={cb.columnsTypeCode} />
-					</div>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">[key: <strong>string</strong>]:</h3>
-						<h3 class="font-bold">Column</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Each key of this object is an accessor in the data object. If a certain key is not
-						present, defaults will be applied for that accessor/column. If a key is present,
-						defaults will be replaced with provided data.
-					</p>
-				</div>
-			</div>
-
-			<div class="grid gap-5">
-				<div class="grid-gap-1" id="column">
-					<h2 class="font-semibold items-center w-max" use:popup={popupHoverColumn}>Column</h2>
-					<div data-popup="popupHoverColumn">
-						<CodeBlock language="ts" code={cb.columnTypeCode} />
-					</div>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">header:</h3>
-						<h3 class="font-bold">string</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Custom header to be displayed for the column. If not provided, header will be the same
-						as the accessor.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">exclude:</h3>
-						<h3 class="font-bold">boolean</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Whether to exclude the column from the table or not. By default, columns are not
-						excluded. An excluded column will still be available in the data object to apply
-						actions.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">colFitlerFn:</h3>
-						<h3 class="font-bold">
-							<a
-								href="https://svelte-headless-table.bryanmylee.com/docs/plugins/add-column-filters#fn-filtervalue-value-boolean"
-								>ColumnFilterFn</a
-							>
-						</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Filtering function to be applied on the column. By default, basic number/string filter
-						is applied.
-					</p>
-				</div>
-
-				<div class="items-center">
-					<div class="flex gap-2">
-						<h3 class="italic">colFilterComponent:</h3>
-						<h3 class="font-bold">SvelteComponent</h3>
-					</div>
-
-					<p class="text-xl pl-10">
-						Custom Svelte component to be used for filtering the column. By default, basic
-						TableFilter component is rendered for the filters.
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="grid gap-20 p-10 relative">
-	<div class="grid grid-cols-2 gap-5 w-full items-start" id="groups">
-		<div class="grid gap-5">
-			<h1>Groups</h1>
-			<Table config={groupConfig} />
 		</div>
 
-		<div class="grid gap-5">
-			<div class="grid gap-10">
-				<TabGroup>
-					<Tab bind:group={groupCodeBlocks} name="group-tab1" value={0}>Usage</Tab>
-					<Tab bind:group={groupCodeBlocks} name="group-tab2" value={1}>Config</Tab>
-					<Tab bind:group={groupCodeBlocks} name="group-tab3" value={2}>Data</Tab>
-					<Tab bind:group={groupCodeBlocks} name="group-tab4" value={3}>Type</Tab>
-					<svelte:fragment slot="panel">
-						{#if groupCodeBlocks === 0}
-							<div class="grid gap-1 grow">
-								<CodeBlock language="html" code={cb.groupHTML} />
-							</div>
-						{:else if groupCodeBlocks === 1}
-							<div class="grid gap-1 grow">
-								<CodeBlock language="ts" code={cb.groupsTableConfigCode} />
-							</div>
-						{:else if groupCodeBlocks === 2}
-							<div class="grid gap-1 grow">
-								<CodeBlock language="ts" code={cb.groupStoreCode} />
-							</div>
-						{:else if groupCodeBlocks === 3}
-							<div class="grid gap-1 grow">
-								<CodeBlock language="ts" code={cb.groupTypeCode} />
-							</div>
-						{/if}
-					</svelte:fragment>
-				</TabGroup>
+		<div class="items-center">
+			<div class="flex gap-2 underline">
+				<div class="italic">data:</div>
+				<div class="font-bold">{`Writable <T[]>`}</div>
 			</div>
+
+			<p class="text-xl pl-10">
+				A writable store of the type <code class="!text-xl">T[]</code>. Any changes in the store
+				will be reflected in the table.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">optionsComponent:</div>
+				<div class="font-bold">{`SvelteComponent`}</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Custom Svelte component to apply actions on a specific row. Table will not have an options
+				column if no <code class="!text-xl">optionsComponent</code> was provided.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">pageSizes:</div>
+				<div class="font-bold">{`number[]`}</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				An array of page sizes to be used for the table. By default, page sizes are 5, 10, 15, 20.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">defaultPageSize:</div>
+				<div class="font-bold">{`number`}</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Default page size to be used for the table upon rendering. By default, default page size is
+				10.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">columns:</div>
+				<div class="font-bold">{`Columns`}</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				An object with configuration for specific columns. <code class="!text-xl">Columns</code>
+				object is described below.
+			</p>
 		</div>
 	</div>
 
-	<div class="grid grid-cols-2 items-start gap-5" id="users">
-		<div class="grid gap-5 grow">
-			<h1>Users</h1>
-			<Table config={usersConfig} />
+	<div class="grid gap-5">
+		<div class="grid gap-2" id="columns">
+			<h3 class="font-semibold items-center w-max">Columns</h3>
+			<button class="btn variant-ghost-primary w-min" use:popup={popupClickColumns}>
+				Show type details</button
+			>
+			<div data-popup="popupClickColumns">
+				<CodeBlock language="ts" code={cb.columnsTypeCode} />
+			</div>
 		</div>
-		<div class="grid gap-1 w-full grow">
-			<p class="text-lg">With only required props:</p>
-			<TabGroup>
-				<Tab bind:group={usersCodeBlocks} name="users-tab1" value={0}>Usage</Tab>
-				<Tab bind:group={usersCodeBlocks} name="users-tab2" value={1}>Config</Tab>
-				<Tab bind:group={usersCodeBlocks} name="users-tab3" value={2}>Data</Tab>
-				<Tab bind:group={usersCodeBlocks} name="users-tab4" value={3}>Type</Tab>
-				<svelte:fragment slot="panel">
-					{#if usersCodeBlocks === 0}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="html" code={cb.usersHTML} />
-						</div>
-					{:else if usersCodeBlocks === 1}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.usersTableConfigCode} />
-						</div>
-					{:else if usersCodeBlocks === 2}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.usersStoreCode} />
-						</div>
-					{:else if usersCodeBlocks === 3}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.userTypeCode} />
-						</div>
-					{/if}
-				</svelte:fragment>
-			</TabGroup>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">[key: <strong>string</strong>]:</div>
+				<div class="font-bold">Column</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Each key of this object is an accessor in the data object. If a certain key is not present,
+				defaults will be applied for that accessor/column. If a key is present, defaults will be
+				replaced with provided data.
+			</p>
 		</div>
 	</div>
 
-	<div class="grid grid-cols-2 items-start gap-5 w-full" id="usersNoRoles">
-		<div class="grid gap-5 grow">
-			<h1>Users <span class="text-xl">Roles excluded</span></h1>
-			<Table config={usersNoRolesConfig} />
+	<div class="grid gap-5">
+		<div class="grid gap-2" id="column">
+			<h3 class="font-semibold items-center w-max">Column</h3>
+			<button class="btn variant-ghost-primary w-min" use:popup={popupClickColumn}>
+				Show type details</button
+			>
+			<div data-popup="popupClickColumn">
+				<CodeBlock language="ts" code={cb.columnTypeCode} />
+			</div>
 		</div>
-		<div class="grid gap-1 w-full">
-			<p class="text-lg">With all possible props:</p>
-			<TabGroup>
-				<Tab bind:group={usersNoRolesCodeBlocks} name="users-noRoles-tab1" value={0}>Usage</Tab>
-				<Tab bind:group={usersNoRolesCodeBlocks} name="users-noRoles-tab2" value={1}>Config</Tab>
-				<Tab bind:group={usersNoRolesCodeBlocks} name="users-noRoles-tab3" value={2}>Data</Tab>
-				<Tab bind:group={usersNoRolesCodeBlocks} name="users-noRoles-tab4" value={3}>Type</Tab>
-				<Tab bind:group={usersNoRolesCodeBlocks} name="users-noRoles-tab5" value={4}
-					>Options Component</Tab
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">header:</div>
+				<div class="font-bold">string</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Custom header to be displayed for the column. If not provided, header will be the same as
+				the accessor.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">exclude:</div>
+				<div class="font-bold">boolean</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Whether to exclude the column from the table or not. By default, columns are not excluded.
+				An excluded column will still be available in the data object to apply actions.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">colFitlerFn:</div>
+				<div class="font-bold">
+					<a
+						href="https://svelte-headless-table.bryanmylee.com/docs/plugins/add-column-filters#fn-filtervalue-value-boolean"
+						>ColumnFilterFn</a
+					>
+				</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Filtering function to be applied on the column. By default, basic number/string filter is
+				applied.
+			</p>
+		</div>
+
+		<div class="items-center">
+			<div class="flex gap-2">
+				<div class="italic">colFilterComponent:</div>
+				<div class="font-bold">SvelteComponent</div>
+			</div>
+
+			<p class="text-xl pl-10">
+				Custom Svelte component to be used for filtering the column. By default, basic TableFilter
+				component is rendered for the filters.
+			</p>
+		</div>
+	</div>
+
+	<div class="grid gap-1">
+		<h2>Examples</h2>
+		<div class="grid gap-20">
+			<div class="grid gap-5" id="groups">
+				<CodeContainer title="Simple table" svelte={cb.groupHTML} data={cb.groupStoreCode}>
+					<Table config={groupConfig} />
+				</CodeContainer>
+			</div>
+
+			<div class="grid gap-5" id="users">
+				<CodeContainer title="Minimal configuration" svelte={cb.usersHTML} data={cb.usersStoreCode}>
+					<Table config={usersConfig} />
+				</CodeContainer>
+			</div>
+
+			<div class="grid gap-5" id="users">
+				<CodeContainer
+					title="Full configuration"
+					svelte={cb.usersNoRolesHTML}
+					data={cb.usersStoreCode}
 				>
-				<svelte:fragment slot="panel">
-					{#if usersNoRolesCodeBlocks === 0}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="html" code={cb.usersNoRolesHTML} />
-						</div>
-					{:else if usersNoRolesCodeBlocks === 1}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.usersNoRolesTableConfigCode} />
-						</div>
-					{:else if usersNoRolesCodeBlocks === 2}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.usersStoreCode} />
-						</div>
-					{:else if usersNoRolesCodeBlocks === 3}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="ts" code={cb.userTypeCode} />
-						</div>
-					{:else if usersNoRolesCodeBlocks === 4}
-						<div class="grid gap-1 grow">
-							<CodeBlock language="html" code={cb.tableOptionsHTML} />
-						</div>
-					{/if}
-				</svelte:fragment>
-			</TabGroup>
+					<Table config={usersNoRolesConfig} />
+				</CodeContainer>
+			</div>
 		</div>
 	</div>
 </div>
--- src/routes/components/table/codeBlocks.ts
+++ src/routes/components/table/codeBlocks.ts
@@ -1,92 +1,103 @@
-export const tableImportCode = `
-import { Table } from 'bexis2-core-ui;`;
+export const groupHTML = `
+<script lang="ts">
+	import { Table } from 'bexis2-core-ui';
+	import type { TableConfig } from 'bexis2-core-ui';
 
-export const groupTypeCode = `
-type Group = {
+	import { groupsStore } from './data';
+	import type { Group } from './data';
+
+	const groupConfig: TableConfig<Group> = {						
+		id: 'userGroups',						
+		data: groupsStore,						
+		columns: {							
+			id: {								
+				header: 'ID',						
+			}						
+		}					
+	};
+</script>
+
+<Table config={groupConfig} />`;
+export const groupStoreCode = `			
+export type Group = {
 	id: number;
 	name: string;
 	description: string
-};`;
+};
 
-export const groupHTML = `<Table config={groupConfig} />`;
-export const groupStoreCode = `				
-const userGroups: Group[] = [{					
-	id: 1, name: 'Group 1', description: 'Testing 1',					
-	id: 2, name: 'Group 2', description: 'Testing 2',					
-	id: 3, name: 'Group 3', description: 'Testing 3',					
-}, ...];					
+const userGroups: Group[] = [					
+	{id: 1, name: 'Group 1', description: 'Testing 1'},					
+	{id: 2, name: 'Group 2', description: 'Testing 2'},					
+	{id: 3, name: 'Group 3', description: 'Testing 3'},					
+	...
+];					
 				
 const groupsStore = writable<Group[]>(userGroups);`;
-export const groupsTableConfigCode = `			
-const groupConfig: TableConfig<Group> = {						
-	id: 'userGroups',						
-	data: groupsStore,						
-	columns: {							
-		id: {								
-			header: 'ID',						
-		}						
-	}					
-};`;
 
-export const userTypeCode = `
-type User = { 
+export const usersHTML = `
+<script lang="ts">
+	import { Table } from 'bexis2-core-ui';
+	import type { TableConfig } from 'bexis2-core-ui';
+	
+	import { usersStore } from './data';
+	import type { User } from './data';
+
+	const usersConfig: TableConfig<User> = {
+		id: 'users',
+		data: usersStore
+	};
+</script>
+
+<Table config={usersConfig} />`;
+export const usersStoreCode = `
+export type User = { 
 	id: number; 
 	name: string; 
 	group: string; 
 	role: string 
-};`;
-export const usersHTML = `<Table config={usersConfig} />`;
-export const usersStoreCode = `				
+};
+
 const users: User[] = [
-	{
-		id: 1,
-		name: 'User 1',
-		group: 'Group 1',
-		role: 'Role 1'
-	},
-	{
-		id: 2,
-		name: 'User 2',
-		group: 'Group 2',
-		role: 'Role 2'
-	},
-	{
-		id: 3,
-		name: 'User 3',
-		group: 'Group 3',
-		role: 'Role 3'
-	},
+	{id: 1, name: 'User 1', group: 'Group 1', role: 'Role 1'},
+	{id: 2, name: 'User 2', group: 'Group 2', role: 'Role 2'},
+	{id: 3, name: 'User 3', group: 'Group 3', role: 'Role 3'},
 	...
 ];				
 				
-const usersStore = writable<User[]>(users);`;
-export const usersTableConfigCode = `
-const usersConfig: TableConfig<User> = {
-	id: 'users',
-	data: usersStore
-};`;
+export const usersStore = writable<User[]>(users);`;
+export const usersNoRolesHTML = `
+<script lang="ts">
+	import { Table } from 'bexis2-core-ui';
+	import type { TableConfig } from 'bexis2-core-ui';
 
-export const usersNoRolesHTML = `<Table config={usersNoRolesConfig} />`;
-export const usersNoRolesTableConfigCode = `
-const usersNoRolesConfig: TableConfig<User> = {
-	id: 'usersNoRoles',
-	data: usersStore,
-	optionsComponent: TableOptions,
-	columns: {
-		id: {
-			colFilterFn: columnFilter,
-			colFilterComponent: TableFilter
+	import TableFilter from './TableFilter.svelte';
+	import TableOptions from './TableOptions.svelte';
+	import { columnFilter } from './filters';
+	import { usersStore } from './data';
+	import type { User } from './data';
+
+	const usersNoRolesConfig: TableConfig<User> = {
+		id: 'usersNoRoles',
+		data: usersStore,
+		optionsComponent: TableOptions,
+		columns: {
+			id: {
+				colFilterFn: columnFilter,
+				colFilterComponent: TableFilter
+			},
+			role: {
+				exclude: true
+			},
+			group: {
+				header: 'Group name'
+			}
 		},
-		role: {
-			exclude: true
-		},
-		group: {
-			header: 'Group name'
-		}
-	},
-	pageSizes: [1, 3, 5],
-	defaultPageSize: 5
-};`;
+		pageSizes: [1, 3, 5],
+		defaultPageSize: 5
+	};
+</script>
+
+<Table config={usersNoRolesConfig} />`;
 
 export const tableOptionsHTML = `
 <script lang="ts">
