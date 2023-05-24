a2a2cbe80999a0350acbe98e5a9e932f219e6869
DavidBlaa <DavidBlaa@users.noreply.github.com>
2023-05-24 09:17:12 +0000

    deploy: 010fb38fc7bd97fb67c27e9c796364ecf95313be

---
+++ _app/immutable/nodes/7.c6913579.js
@@ -0,0 +1,87 @@
+import{S as Qt,i as Xt,s as Zt,k as o,y as b,a as d,q as h,l as s,m as f,z as A,c,r as m,h as a,n as bt,b as At,A as D,D as t,W as xt,g as S,d as R,B as x}from"../chunks/index.cff9c354.js";import{P as De,C as Te}from"../chunks/CodeContainer.2fbc6041.js";import{F as Y,f as be,a as Ae,b as Se,c as Re,d as xe,e as Ie,g as Fe}from"../chunks/Table.097fc0e9.js";function ye(I){let e,r,n,l,$,C,E,T,i,g,u,P,J,p,F,y,H,O,N,K,M,U;return r=new Y({props:{icon:be}}),i=new Y({props:{icon:Ae}}),y=new Y({props:{icon:Se}}),{c(){e=o("div"),b(r.$$.fragment),n=d(),l=o("span"),$=o("b"),C=h("faAngleRight"),E=h(" - fa-angle-right"),T=d(),b(i.$$.fragment),g=d(),u=o("span"),P=o("b"),J=h("faAngleDown"),p=h(" - fa-angle-down"),F=d(),b(y.$$.fragment),H=d(),O=o("span"),N=o("b"),K=h("faAddressBook"),M=h(" - fa-adress-book"),this.h()},l(w){e=s(w,"DIV",{class:!0});var _=f(e);A(r.$$.fragment,_),n=c(_),l=s(_,"SPAN",{});var k=f(l);$=s(k,"B",{});var G=f($);C=m(G,"faAngleRight"),G.forEach(a),E=m(k," - fa-angle-right"),k.forEach(a),T=c(_),A(i.$$.fragment,_),g=c(_),u=s(_,"SPAN",{});var j=f(u);P=s(j,"B",{});var et=f(P);J=m(et,"faAngleDown"),et.forEach(a),p=m(j," - fa-angle-down"),j.forEach(a),F=c(_),A(y.$$.fragment,_),H=c(_),O=s(_,"SPAN",{});var V=f(O);N=s(V,"B",{});var nt=f(N);K=m(nt,"faAddressBook"),nt.forEach(a),M=m(V," - fa-adress-book"),V.forEach(a),_.forEach(a),this.h()},h(){bt(e,"class","grid grid-cols-2")},m(w,_){At(w,e,_),D(r,e,null),t(e,n),t(e,l),t(l,$),t($,C),t(l,E),t(e,T),D(i,e,null),t(e,g),t(e,u),t(u,P),t(P,J),t(u,p),t(e,F),D(y,e,null),t(e,H),t(e,O),t(O,N),t(N,K),t(O,M),U=!0},p:xt,i(w){U||(S(r.$$.fragment,w),S(i.$$.fragment,w),S(y.$$.fragment,w),U=!0)},o(w){R(r.$$.fragment,w),R(i.$$.fragment,w),R(y.$$.fragment,w),U=!1},d(w){w&&a(e),x(r),x(i),x(y)}}}class ke extends Qt{constructor(e){super(),Xt(this,e,null,ye,Zt,{})}}const Pe=`<script>
+ import Fa from 'svelte-fa/src/fa.svelte'
+
+ import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+ import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+ import { faAddressBook } from '@fortawesome/free-solid-svg-icons'
+
+
+
+<\/script>
+
+
+<div class="grid grid-cols-2">
+
+ 
+  <Fa icon={faAngleRight} />
+  <span><b>faAngleRight</b> - fa-angle-right</span>
+  
+  <Fa icon={faAngleDown}/>
+  <span><b>faAngleDown</b> - fa-angle-down</span>
+
+  <Fa icon={faAddressBook}/>
+  <span><b>faAddressBook</b> - fa-adress-book  </span>
+
+</div>
+
+
+`;function Be(I){let e,r,n,l,$,C,E,T,i,g,u,P,J,p,F,y,H,O,N,K,M,U,w,_,k,G,j,et,V,nt,It,ct,Ft,yt,q,it,Q,kt,ht,Pt,Bt,mt,Ct,Ht,z,$t,X,Nt,gt,jt,Ot,pt,Ut,Vt,L,vt,Z,qt,ut,zt,Lt,_t,Wt,Yt,W,wt,tt,Gt,Et,Jt,Kt,Tt,Mt,Dt;return H=new Y({props:{icon:Ae}}),j=new Y({props:{icon:be}}),Q=new Y({props:{icon:Re}}),X=new Y({props:{icon:xe}}),Z=new Y({props:{icon:Ie}}),tt=new Y({props:{icon:Fe}}),{c(){e=o("div"),r=o("table"),n=o("thead"),l=o("tr"),$=o("th"),C=h("Icon"),E=d(),T=o("th"),i=h("Id"),g=d(),u=o("th"),P=h("Case"),J=d(),p=o("tbody"),F=o("tr"),y=o("td"),b(H.$$.fragment),O=d(),N=o("td"),K=h("faAngleDown"),M=d(),U=o("td"),w=h("Expand"),_=d(),k=o("tr"),G=o("td"),b(j.$$.fragment),et=d(),V=o("td"),nt=h("faAngleRight"),It=d(),ct=o("td"),Ft=h("Collapse"),yt=d(),q=o("tr"),it=o("td"),b(Q.$$.fragment),kt=d(),ht=o("td"),Pt=h("faSave"),Bt=d(),mt=o("td"),Ct=h("Save"),Ht=d(),z=o("tr"),$t=o("td"),b(X.$$.fragment),Nt=d(),gt=o("td"),jt=h("faEye"),Ot=d(),pt=o("td"),Ut=h("Show"),Vt=d(),L=o("tr"),vt=o("td"),b(Z.$$.fragment),qt=d(),ut=o("td"),zt=h("faPen"),Lt=d(),_t=o("td"),Wt=h("Edit"),Yt=d(),W=o("tr"),wt=o("td"),b(tt.$$.fragment),Gt=d(),Et=o("td"),Jt=h("faTrash"),Kt=d(),Tt=o("td"),Mt=h("Delete"),this.h()},l(v){e=s(v,"DIV",{});var St=f(e);r=s(St,"TABLE",{class:!0});var Rt=f(r);n=s(Rt,"THEAD",{});var te=f(n);l=s(te,"TR",{});var at=f(l);$=s(at,"TH",{});var ee=f($);C=m(ee,"Icon"),ee.forEach(a),E=c(at),T=s(at,"TH",{});var ne=f(T);i=m(ne,"Id"),ne.forEach(a),g=c(at),u=s(at,"TH",{});var ae=f(u);P=m(ae,"Case"),ae.forEach(a),at.forEach(a),te.forEach(a),J=c(Rt),p=s(Rt,"TBODY",{});var B=f(p);F=s(B,"TR",{});var rt=f(F);y=s(rt,"TD",{});var re=f(y);A(H.$$.fragment,re),re.forEach(a),O=c(rt),N=s(rt,"TD",{});var oe=f(N);K=m(oe,"faAngleDown"),oe.forEach(a),M=c(rt),U=s(rt,"TD",{});var se=f(U);w=m(se,"Expand"),se.forEach(a),rt.forEach(a),_=c(B),k=s(B,"TR",{});var ot=f(k);G=s(ot,"TD",{});var fe=f(G);A(j.$$.fragment,fe),fe.forEach(a),et=c(ot),V=s(ot,"TD",{});var le=f(V);nt=m(le,"faAngleRight"),le.forEach(a),It=c(ot),ct=s(ot,"TD",{});var de=f(ct);Ft=m(de,"Collapse"),de.forEach(a),ot.forEach(a),yt=c(B),q=s(B,"TR",{});var st=f(q);it=s(st,"TD",{});var ce=f(it);A(Q.$$.fragment,ce),ce.forEach(a),kt=c(st),ht=s(st,"TD",{});var ie=f(ht);Pt=m(ie,"faSave"),ie.forEach(a),Bt=c(st),mt=s(st,"TD",{});var he=f(mt);Ct=m(he,"Save"),he.forEach(a),st.forEach(a),Ht=c(B),z=s(B,"TR",{});var ft=f(z);$t=s(ft,"TD",{});var me=f($t);A(X.$$.fragment,me),me.forEach(a),Nt=c(ft),gt=s(ft,"TD",{});var $e=f(gt);jt=m($e,"faEye"),$e.forEach(a),Ot=c(ft),pt=s(ft,"TD",{});var ge=f(pt);Ut=m(ge,"Show"),ge.forEach(a),ft.forEach(a),Vt=c(B),L=s(B,"TR",{});var lt=f(L);vt=s(lt,"TD",{});var pe=f(vt);A(Z.$$.fragment,pe),pe.forEach(a),qt=c(lt),ut=s(lt,"TD",{});var ve=f(ut);zt=m(ve,"faPen"),ve.forEach(a),Lt=c(lt),_t=s(lt,"TD",{});var ue=f(_t);Wt=m(ue,"Edit"),ue.forEach(a),lt.forEach(a),Yt=c(B),W=s(B,"TR",{});var dt=f(W);wt=s(dt,"TD",{});var _e=f(wt);A(tt.$$.fragment,_e),_e.forEach(a),Gt=c(dt),Et=s(dt,"TD",{});var we=f(Et);Jt=m(we,"faTrash"),we.forEach(a),Kt=c(dt),Tt=s(dt,"TD",{});var Ee=f(Tt);Mt=m(Ee,"Delete"),Ee.forEach(a),dt.forEach(a),B.forEach(a),Rt.forEach(a),St.forEach(a),this.h()},h(){bt(r,"class","table table-hover")},m(v,St){At(v,e,St),t(e,r),t(r,n),t(n,l),t(l,$),t($,C),t(l,E),t(l,T),t(T,i),t(l,g),t(l,u),t(u,P),t(r,J),t(r,p),t(p,F),t(F,y),D(H,y,null),t(F,O),t(F,N),t(N,K),t(F,M),t(F,U),t(U,w),t(p,_),t(p,k),t(k,G),D(j,G,null),t(k,et),t(k,V),t(V,nt),t(k,It),t(k,ct),t(ct,Ft),t(p,yt),t(p,q),t(q,it),D(Q,it,null),t(q,kt),t(q,ht),t(ht,Pt),t(q,Bt),t(q,mt),t(mt,Ct),t(p,Ht),t(p,z),t(z,$t),D(X,$t,null),t(z,Nt),t(z,gt),t(gt,jt),t(z,Ot),t(z,pt),t(pt,Ut),t(p,Vt),t(p,L),t(L,vt),D(Z,vt,null),t(L,qt),t(L,ut),t(ut,zt),t(L,Lt),t(L,_t),t(_t,Wt),t(p,Yt),t(p,W),t(W,wt),D(tt,wt,null),t(W,Gt),t(W,Et),t(Et,Jt),t(W,Kt),t(W,Tt),t(Tt,Mt),Dt=!0},p:xt,i(v){Dt||(S(H.$$.fragment,v),S(j.$$.fragment,v),S(Q.$$.fragment,v),S(X.$$.fragment,v),S(Z.$$.fragment,v),S(tt.$$.fragment,v),Dt=!0)},o(v){R(H.$$.fragment,v),R(j.$$.fragment,v),R(Q.$$.fragment,v),R(X.$$.fragment,v),R(Z.$$.fragment,v),R(tt.$$.fragment,v),Dt=!1},d(v){v&&a(e),x(H),x(j),x(Q),x(X),x(Z),x(tt)}}}class Ce extends Qt{constructor(e){super(),Xt(this,e,null,Be,Zt,{})}}const He=`<script>
+ import Fa from 'svelte-fa/src/fa.svelte'
+
+ import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+ import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+ import { faSave } from '@fortawesome/free-solid-svg-icons'
+ import { faEye } from '@fortawesome/free-solid-svg-icons'
+ import { faPen } from '@fortawesome/free-solid-svg-icons'
+ import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+<\/script>
+<div>
+<table class="table table-hover">
+ <thead>
+   <tr>
+     <th>Icon</th>
+     <th>Id</th>
+     <th>Case</th>
+   </tr>
+ </thead>
+ <tbody>
+   <tr>
+     <td><Fa icon={faAngleDown} /></td>
+     <td>faAngleDown</td>
+     <td>Expand</td>
+
+   </tr>
+   <tr>
+     <td><Fa icon={faAngleRight} /></td>
+     <td>faAngleRight</td>
+     <td>Collapse</td>
+   </tr>
+   <tr>
+     <td><Fa icon={faSave} /></td>
+     <td>faSave</td>
+     <td>Save</td>
+
+   </tr>
+   <tr>
+     <td><Fa icon={faEye} /></td>
+     <td>faEye</td>
+     <td>Show</td>
+   </tr>
+   <tr>
+     <td><Fa icon={faPen} /></td>
+     <td>faPen</td>
+     <td>Edit</td>
+
+   </tr>
+   <tr>
+     <td><Fa icon={faTrash} /></td>
+     <td>faTrash</td>
+     <td>Delete</td>
+   </tr>
+ </tbody>
+</table>
+</div>
+
+`;function Ne(I){let e,r;return e=new ke({}),{c(){b(e.$$.fragment)},l(n){A(e.$$.fragment,n)},m(n,l){D(e,n,l),r=!0},i(n){r||(S(e.$$.fragment,n),r=!0)},o(n){R(e.$$.fragment,n),r=!1},d(n){x(e,n)}}}function je(I){let e,r;return{c(){e=o("span"),r=h("This example shows how to set a Icon."),this.h()},l(n){e=s(n,"SPAN",{slot:!0});var l=f(e);r=m(l,"This example shows how to set a Icon."),l.forEach(a),this.h()},h(){bt(e,"slot","info")},m(n,l){At(n,e,l),t(e,r)},p:xt,d(n){n&&a(e)}}}function Oe(I){let e,r;return e=new Ce({}),{c(){b(e.$$.fragment)},l(n){A(e.$$.fragment,n)},m(n,l){D(e,n,l),r=!0},i(n){r||(S(e.$$.fragment,n),r=!0)},o(n){R(e.$$.fragment,n),r=!1},d(n){x(e,n)}}}function Ue(I){let e,r;return{c(){e=o("span"),r=h("This story shows alle used icons in bexis 2."),this.h()},l(n){e=s(n,"SPAN",{slot:!0});var l=f(e);r=m(l,"This story shows alle used icons in bexis 2."),l.forEach(a),this.h()},h(){bt(e,"slot","info")},m(n,l){At(n,e,l),t(e,r)},p:xt,d(n){n&&a(e)}}}function Ve(I){let e,r,n,l,$,C,E,T;return $=new Te({props:{title:"SetIcon",svelte:Pe,external:[{label:"Free Solid Awesome Font Icons v5",url:"https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free"},{label:"https://www.npmjs.com/package/svelte-fa",url:"https://www.npmjs.com/package/svelte-fa"}],$$slots:{info:[je],default:[Ne]},$$scope:{ctx:I}}}),E=new Te({props:{title:"Used Icons",svelte:He,external:[{label:"Free Solid Awesome Font Icons v5",url:"https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free"},{label:"https://www.npmjs.com/package/svelte-fa",url:"https://www.npmjs.com/package/svelte-fa"}],$$slots:{info:[Ue],default:[Oe]},$$scope:{ctx:I}}}),{c(){e=o("div"),r=o("h2"),n=h("Icon Overview"),l=d(),b($.$$.fragment),C=d(),b(E.$$.fragment),this.h()},l(i){e=s(i,"DIV",{id:!0});var g=f(e);r=s(g,"H2",{});var u=f(r);n=m(u,"Icon Overview"),u.forEach(a),l=c(g),A($.$$.fragment,g),C=c(g),A(E.$$.fragment,g),g.forEach(a),this.h()},h(){bt(e,"id","toc-target")},m(i,g){At(i,e,g),t(e,r),t(r,n),t(e,l),D($,e,null),t(e,C),D(E,e,null),T=!0},p(i,g){const u={};g&1&&(u.$$scope={dirty:g,ctx:i}),$.$set(u);const P={};g&1&&(P.$$scope={dirty:g,ctx:i}),E.$set(P)},i(i){T||(S($.$$.fragment,i),S(E.$$.fragment,i),T=!0)},o(i){R($.$$.fragment,i),R(E.$$.fragment,i),T=!1},d(i){i&&a(e),x($),x(E)}}}function qe(I){let e,r;return e=new De({props:{$$slots:{default:[Ve]},$$scope:{ctx:I}}}),{c(){b(e.$$.fragment)},l(n){A(e.$$.fragment,n)},m(n,l){D(e,n,l),r=!0},p(n,[l]){const $={};l&1&&($.$$scope={dirty:l,ctx:n}),e.$set($)},i(n){r||(S(e.$$.fragment,n),r=!0)},o(n){R(e.$$.fragment,n),r=!1},d(n){x(e,n)}}}class Ye extends Qt{constructor(e){super(),Xt(this,e,null,qe,Zt,{})}}export{Ye as component};
