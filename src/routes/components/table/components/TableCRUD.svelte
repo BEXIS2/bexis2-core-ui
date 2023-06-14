de73b4c97fb27729f3b1fabcf161f8e8aec63eac
Ibrahim Mammadov <zxyctn@gmail.com>
2023-06-14 10:39:47 +0200

    Add TableCRUD options component

---
+++ src/routes/components/table/components/TableCRUD.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faPlus, faEye, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+	export let row;
+	export let dispatchFn;
+
+	const eventDispatchFn = (type: string) => {
+		return dispatchFn({ type, row });
+	};
+
+	const buttons = [
+		{
+			icon: faPlus,
+			color: 'variant-filled-primary',
+			type: 'CREATE'
+		},
+		{
+			icon: faEye,
+			color: 'variant-filled-secondary',
+			type: 'READ'
+		},
+		{
+			icon: faPen,
+			color: 'variant-filled-warning',
+			type: 'UPDATE'
+		},
+		{
+			icon: faTrash,
+			color: 'variant-filled-error',
+			type: 'DELETE'
+		}
+	];
+</script>
+
+<div class="flex gap-2 w-min">
+	{#each buttons as button}
+		<button
+			class={`btn btn-sm btn-icon rounded-md ${button.color}`}
+			on:click|preventDefault={() => eventDispatchFn(button.type)}
+		>
+			<Fa icon={button.icon} />
+		</button>
+	{/each}
+</div>
