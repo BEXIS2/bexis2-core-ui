e05d86944f639c0146806bc6441d5eac58139bb9
Ibrahim Mammadov <zxyctn@gmail.com>
2023-06-14 10:40:08 +0200

    Add TableCRUD example

--- src/routes/components/table/docs/TableExamplesDocs.svelte
+++ src/routes/components/table/docs/TableExamplesDocs.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
+	import { CodeBlock, Modal, modalStore } from '@skeletonlabs/skeleton';
 
 	import CodeContainer from '$docs/components/CodeContainer.svelte';
 	import Table from '$lib/components/Table/Table.svelte';
-	import TableOptions from './TableOptions.svelte';
 	import TableFilter from '$lib/components/Table/TableFilter.svelte';
+	import TableOptions from '../components/TableOptions.svelte';
+	import TableCrud from '../components/TableCRUD.svelte';
+	import AddUser from '../components/AddUser.svelte';
+	import ShowUsers from '../components/ShowUsers.svelte';
+	import EditGroup from '../components/EditGroup.svelte';
+	import CustomCodeContainer from '../components/CustomCodeContainer.svelte';
+	import * as data from '../data/data';
+	import * as cb from '../data/codeBlocks';
 	import { columnFilter } from '$lib/components/Table/filter';
-	import * as data from './data';
-	import * as cb from './codeBlocks';
 	import type { TableConfig } from '$lib/models/Models';
 
 	type Group = { id: number; name: string; description: string };
@@ -120,6 +126,69 @@
 			}
 		}
 	};
+
+	const tableCRUDConfig: TableConfig<Group> = {
+		id: 'userGroupCRUD',
+		data: groupsStore,
+		optionsComponent: TableCrud
+	};
+
+	const tableCRUDActions = (action: CustomEvent<{ row: Group; type: string }>) => {
+		const { type, row } = action.detail;
+		switch (type) {
+			case 'CREATE':
+				modalStore.trigger({
+					type: 'component',
+					title: `Add user to Group ${row.id}`,
+					component: {
+						ref: AddUser,
+						props: { users: $usersStore.filter((user) => user.group !== row.name), group: row.name }
+					}
+				});
+				break;
+			case 'READ':
+				modalStore.trigger({
+					type: 'component',
+					title: `Users in Group ${row.id}`,
+					component: {
+						ref: ShowUsers,
+						props: { users: $usersStore.filter((user) => user.group === row.name), group: row.name }
+					}
+				});
+				break;
+			case 'UPDATE':
+				modalStore.trigger({
+					type: 'component',
+					title: `Edit Group ${row.id}`,
+					component: {
+						ref: EditGroup,
+						props: { group: row }
+					}
+				});
+				break;
+			case 'DELETE':
+				modalStore.trigger({
+					type: 'confirm',
+					title: 'Group deletion',
+					body: `Are you sure you want to delete <strong>${row.name}</strong>?`,
+					response: (r: boolean) => console.log('response:', r)
+				});
+				break;
+
+			default:
+				break;
+		}
+	};
+
+	const tableCRUDtabs = [
+		{ name: 'Svelte', language: 'html', code: cb.tableCRUDHTML },
+		{ name: 'tableCRUDActions', language: 'typescript', code: cb.tableCRUDActionsCode },
+		{ name: 'optionsComponent', language: 'html', code: cb.tableCRUDOptionsHTML },
+		{ name: 'data', language: 'typescript', code: cb.usersStoreCode + '\n' + cb.groupStoreCode },
+		{ name: 'AddUser.svelte', language: 'html', code: cb.tableCRUDAddUserHTML },
+		{ name: 'ShowUsers.svelte', language: 'html', code: cb.tableCRUDShowUsersHTML },
+		{ name: 'EditGroup.svelte', language: 'html', code: cb.tableCRUDEditGroupHTML }
+	];
 </script>
 
 <div class="grid gap-1">
@@ -170,14 +239,27 @@
 			</CodeContainer>
 		</div>
 
-		<div class="grid gap-5" id="usersNoRoles">
-			<CodeContainer
-				title="Full configuration"
-				svelte={cb.usersNoRolesHTML}
-				data={cb.usersStoreCode}
-			>
-				<Table config={usersNoRolesConfig} on:action={(obj) => alert(obj.detail.type)} />
-			</CodeContainer>
+		<div id="actionDispatchersExamples" class="mb-20">
+			<h2>Dispatching Actions</h2>
+			<div class="grid gap-5" id="usersNoRoles">
+				<CodeContainer title="Odd or even" svelte={cb.usersNoRolesHTML} data={cb.usersStoreCode}>
+					<div class="grid gap-10">
+						<Table config={usersNoRolesConfig} on:action={(obj) => alert(obj.detail.type)} />
+						<div class="grid gap-5">
+							<h3>TableOptions component</h3>
+							<CodeBlock language="html" code={cb.tableOptionsHTML} />
+						</div>
+					</div>
+				</CodeContainer>
+			</div>
+
+			<div class="grid gap-5" id="CRUD">
+				<CustomCodeContainer title="CRUD" tabs={tableCRUDtabs}>
+					<Table config={tableCRUDConfig} on:action={tableCRUDActions} />
+				</CustomCodeContainer>
+			</div>
+
+			<!-- --------------------------------------- -->
 		</div>
 	</div>
 	<div id="complexTypes">
@@ -215,3 +297,5 @@
 		</div>
 	</div>
 </div>
+
+<Modal />
