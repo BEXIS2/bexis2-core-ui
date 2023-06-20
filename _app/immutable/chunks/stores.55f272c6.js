d66739a5ffc368b69e661ce75bec0dc6fd7293be
DavidBlaa <DavidBlaa@users.noreply.github.com>
2023-06-20 06:10:36 +0000

    deploy: 840eb26e8517c9187cd1606e259cb39be2fcd635

---
+++ _app/immutable/chunks/stores.55f272c6.js
@@ -0,0 +1,1 @@
+import{s as e}from"./singletons.786e2d82.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
