f2a438095016408c2ab28fa1322d910a6e80ec4e
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-18 12:21:05 +0400

    Remove x-axis scrollbar from TableOfContents

--- src/routes/components/table/+layout.svelte
+++ src/routes/components/table/+layout.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <AppShell
-	slotSidebarLeft="flex-none overflow-x-hidden overflow-y-auto bg-surface-50-900-token lg:w-auto relative"
+	slotSidebarLeft="flex-none bg-surface-50-900-token lg:w-auto relative"
 >
 	<svelte:fragment slot="sidebarRight">
-		<div class="fixed right-0 top-0 overflow-auto h-full" class:w-10={closed} class:w-max={!closed}>
-			<div class="px-10 pt-10 text-primary-500 overflow-auto h-max bg-white/95">
+		<div class="fixed right-0 top-0 overflow-y-auto h-full" class:w-10={closed} class:w-max={!closed}>
+			<div class="px-10 pt-10 text-primary-500 h-max overflow-x-hidden bg-white/95">
 				<button
 					class="fixed z-30 top-0 right-0 btn btn-sm variant-filled-primary m-1"
 					on:click={() => (closed = !closed)}
