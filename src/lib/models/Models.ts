40413f8eb779e05415c61fe39c82769db6e9ec58
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-18 11:03:58 +0200

    Remove fitToScreen from TableConfig

--- src/lib/models/Models.ts
+++ src/lib/models/Models.ts
@@ -81,7 +81,6 @@ export interface Columns {
 export interface TableConfig<T> {
 	id: string;
 	data: Writable<T[]>;
-	fitToScreen: boolean;
 	columns?: Columns;
 	pageSizes?: number[];
 	defaultPageSize?: number;
