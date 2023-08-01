5dc8d8b7903044a87d1b916243208eca83a2a7fb
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-01 15:53:08 +0200

    Update viteConfig for codemirror dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ export default defineConfig({
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
+	optimizeDeps: {
+		exclude: [
+			'codemirror',
+			'@codemirror/language-javascript',
+			'@codemirror/lang-html',
+			'@codemirror/theme-one-dark'
+		]
+	},
 	build: {
 		chunkSizeWarningLimit: 1600
 	}
