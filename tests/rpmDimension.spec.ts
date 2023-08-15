ecbc3acd3b2a2336e5cfefe851c97ffe63d86b0d
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-15 16:38:39 +0200

    Optimize code

--- tests/rpmDimension.spec.ts
+++ tests/rpmDimension.spec.ts
@@ -6,8 +6,8 @@ test.describe('RPM Dimension', () => {
 	// Declare page outside of the test hooks so it's accessible by all tests.
 	let page: Page;
 
-	test.beforeAll(async ({ context }) => {
-		page = await context.newPage(); // Create new page
+	test.beforeAll(async ({ browser }) => {
+		page = await browser.newPage(); // Create new page
 		await login(page); // Login
 		await page.goto(`${host}/rpm/dimension/`); // Go to DimensionManager page
 	});
@@ -55,15 +55,11 @@ test.describe('RPM Dimension', () => {
 
 		test('Find the new dimension in the table', async () => {
 			await page.locator('.table-container').getByPlaceholder('Search rows...').fill(dimension);
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').first()
-			).toHaveText(dimension);
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').nth(1)
-			).toHaveText('Test dimension');
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').nth(2)
-			).toHaveText('L(0,0)M(0,0)T(0,0)I(0,0)Θ(0,0)N(0,0)J(0,1)');
+
+			const cols = page.locator('.table.table-compact').locator('tr').locator('td');
+			await expect(cols.first()).toHaveText(dimension);
+			await expect(cols.nth(1)).toHaveText('Test dimension');
+			await expect(cols.nth(2)).toHaveText('L(0,0)M(0,0)T(0,0)I(0,0)Θ(0,0)N(0,0)J(0,1)');
 		});
 	});
 
@@ -98,15 +94,11 @@ test.describe('RPM Dimension', () => {
 
 		test('Find the edited dimension in the table', async () => {
 			await page.locator('.table-container').getByPlaceholder('Search rows...').fill(dimension);
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').first()
-			).toHaveText(dimension);
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').nth(1)
-			).toHaveText('Test dimension edited');
-			await expect(
-				page.locator('.table.table-compact').locator('tr').locator('td').nth(2)
-			).toHaveText('L(0,0)M(0,0)T(0,0)I(0,0)Θ(0,0)N(0,0)J(0,1)');
+
+			const cols = await page.locator('.table.table-compact').locator('tr').locator('td');
+			await expect(cols.first()).toHaveText(dimension);
+			await expect(cols.nth(1)).toHaveText('Test dimension edited');
+			await expect(cols.nth(2)).toHaveText('L(0,0)M(0,0)T(0,0)I(0,0)Θ(0,0)N(0,0)J(0,1)');
 		});
 	});
 
