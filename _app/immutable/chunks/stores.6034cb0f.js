d60cf3c1d7dd9b44a72c23514318ee05f5dc4c3f
sventhiel <sventhiel@users.noreply.github.com>
2023-09-11 09:32:08 +0000

    deploy: ab59eaeb0999a73cf7bd65c2a2892c00ffbbb23f

--- _app/immutable/chunks/stores.6034cb0f.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.a14d9287.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
