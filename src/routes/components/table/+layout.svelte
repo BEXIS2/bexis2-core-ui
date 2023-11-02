ab26386f33b676be6fa6a9cbe143991e4b9be47f
Ibrahim Mammadov <zxyctn@gmail.com>
2023-11-02 12:03:43 +0100

    Fix tableOfContents scrollbars

--- src/routes/components/table/+layout.svelte
+++ src/routes/components/table/+layout.svelte
@@ -7,11 +7,11 @@
 <AppShell slotSidebarLeft="flex-none bg-surface-50-900-token lg:w-auto relative">
 	<svelte:fragment slot="sidebarRight">
 		<div
-			class="fixed right-0 top-0 overflow-y-auto h-full"
+			class="fixed right-0 top-0 overflow-y-auto h-full overflow-x-hidden"
 			class:w-10={closed}
 			class:w-max={!closed}
 		>
-			<div class="px-10 pt-10 text-primary-500 h-max overflow-x-hidden bg-white/95">
+			<div class="px-10 pt-10 text-primary-500 h-max transition-all duration-100 ease-out {closed ? '' : 'bg-white/95'}">
 				<button
 					class="fixed z-30 top-0 right-0 btn btn-sm variant-filled-primary m-1"
 					on:click={() => (closed = !closed)}
