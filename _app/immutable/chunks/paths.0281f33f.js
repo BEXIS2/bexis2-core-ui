0e3a4b936e2cdba22a5ddb2ce2f7f9d402a99fcf
geofranzi <geofranzi@users.noreply.github.com>
2023-11-10 16:31:04 +0000

    deploy: 77ad300555b1293cdc6ab25f6a891a08381716ee

---
+++ _app/immutable/chunks/paths.0281f33f.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1ackdzn)==null?void 0:s.base)??"/bexis2-core-ui";var a;const t=((a=globalThis.__sveltekit_1ackdzn)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/chunks/paths.d27352ce.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1bjtihr)==null?void 0:s.base)??"/bexis2-core-ui";var e;const a=((e=globalThis.__sveltekit_1bjtihr)==null?void 0:e.assets)??t;export{a,t as b};
