b79dc32e2fff1dccc40a553d00d911df6bc96b2a
Ibrahim Mammadov <zxyctn@gmail.com>
2024-01-24 12:11:03 +0100

    Add page indicator and input field for table

--- src/lib/components/Table/TablePagination.svelte
+++ src/lib/components/Table/TablePagination.svelte
@@ -18,49 +18,85 @@
 	const goToNextPage = () => ($pageIndex += 1);
 	const goToPreviousPage = () => ($pageIndex -= 1);
 
+	// Handles the input change event
+	const handleChange = (e) => {
+		const value = e.target.value;
+
+		if (value > $pageCount) {
+			$pageIndex = $pageCount - 1;
+		} else if (value < 1) {
+			$pageIndex = 0;
+		} else {
+			$pageIndex = value - 1;
+		}
+	};
+
 	$: goToFirstPageDisabled = !$pageIndex;
 	$: goToLastPageDisabled = $pageIndex == $pageCount - 1;
 	$: goToNextPageDisabled = !$hasNextPage;
 	$: goToPreviousPageDisabled = !$hasPreviousPage;
 </script>
 
-<div class="flex justify-center gap-1">
-	<button
-		class="btn btn-sm variant-filled-primary"
-		on:click|preventDefault={goToFirstPage}
-		disabled={goToFirstPageDisabled}
-		id="{id}-first"
-	>
-		<Fa icon={faAnglesLeft} /></button
-	>
-	<button
-		class="btn btn-sm variant-filled-primary"
-		id="{id}-previous"
-		on:click|preventDefault={goToPreviousPage}
-		disabled={goToPreviousPageDisabled}><Fa icon={faAngleLeft} /></button
-	>
-
-	<select
-		name="pageSize"
-		id="{id}-pageSize"
-		class="select variant-filled-primary w-min font-bold"
-		bind:value={$pageSize}
-	>
-		{#each pageSizes as size}
-			<option value={size}>{size}</option>
-		{/each}
-	</select>
-
-	<button
-		class="btn btn-sm variant-filled-primary"
-		id="{id}-next"
-		on:click|preventDefault={goToNextPage}
-		disabled={goToNextPageDisabled}><Fa icon={faAngleRight} /></button
-	>
-	<button
-		class="btn btn-sm variant-filled-primary"
-		id="{id}-last"
-		on:click|preventDefault={goToLastPage}
-		disabled={goToLastPageDisabled}><Fa icon={faAnglesRight} /></button
-	>
+<div class="flex justify-between w-full items-stretch gap-10">
+	<div class="flex justify-start">
+		<select
+			name="pageSize"
+			id="{id}-pageSize"
+			class="select variant-filled-primary w-min font-bold"
+			bind:value={$pageSize}
+		>
+			{#each pageSizes as size}
+				<option value={size}>{size}</option>
+			{/each}
+		</select>
+	</div>
+	<div class="flex justify-center gap-1">
+		<button
+			class="btn btn-sm variant-filled-primary"
+			on:click|preventDefault={goToFirstPage}
+			disabled={goToFirstPageDisabled}
+			id="{id}-first"
+		>
+			<Fa icon={faAnglesLeft} /></button
+		>
+		<button
+			class="btn btn-sm variant-filled-primary"
+			id="{id}-previous"
+			on:click|preventDefault={goToPreviousPage}
+			disabled={goToPreviousPageDisabled}><Fa icon={faAngleLeft} /></button
+		>
+		<input
+			type="number"
+			class="input border border-primary-500 rounded flex w-24"
+			value={$pageIndex + 1}
+			max={$pageCount}
+			min={1}
+			on:change={handleChange}
+		/>
+		<button
+			class="btn btn-sm variant-filled-primary"
+			id="{id}-next"
+			on:click|preventDefault={goToNextPage}
+			disabled={goToNextPageDisabled}><Fa icon={faAngleRight} /></button
+		>
+		<button
+			class="btn btn-sm variant-filled-primary"
+			id="{id}-last"
+			on:click|preventDefault={goToLastPage}
+			disabled={goToLastPageDisabled}><Fa icon={faAnglesRight} /></button
+		>
+	</div>
+	<div class="flex justify-end items-center">
+		<span class="text-sm text-gray-500">
+			{#if $pageCount > 0}
+				{#if $pageCount == 1}
+					1 page
+				{:else}
+					{$pageCount} pages
+				{/if}
+			{:else}
+				No pages
+			{/if}
+		</span>
+	</div>
 </div>
