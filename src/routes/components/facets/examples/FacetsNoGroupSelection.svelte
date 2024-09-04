96c528465e947aa062d83409235a8b5865b26807
Ibrahim Mammadov <zxyctn@gmail.com>
2024-09-04 14:19:26 +0200

    Update the examples to use writable stores instead of arrays for groups

--- src/routes/components/facets/examples/FacetsNoGroupSelection.svelte
+++ src/routes/components/facets/examples/FacetsNoGroupSelection.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
+
 	import CodeContainer from '$docs/components/CodeContainer.svelte';
 	import Facets from '$lib/components/Facets/Facets.svelte';
 	import { facetsNoGroupSelectionData, facetsNoGroupSelectionSvelte } from '../data/codeBlocks';
 	import { groups } from '../data/data';
 
-	let selected = {
-		Mediums: [],
-		Genres: [],
-		Authors: []
-	};
+	const groupsStore = writable(groups);
 </script>
 
 <div id="facetsNoGroupSelection">
-	<CodeContainer title="No group selection, closed by default and all options displayed in UI" svelte={facetsNoGroupSelectionSvelte} data={facetsNoGroupSelectionData}>
-		<Facets {groups} bind:selected showAll />
+	<CodeContainer
+		title="No group selection, closed by default and all options displayed in UI"
+		svelte={facetsNoGroupSelectionSvelte}
+		data={facetsNoGroupSelectionData}
+	>
+		<Facets groups={groupsStore} showAll on:change={(e) => console.log(e)} />
 	</CodeContainer>
 </div>
