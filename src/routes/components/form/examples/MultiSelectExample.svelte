968c7614c2fef6bff1a731ff7ab519a0d2772ddd
david schoene <david.schoene@uni-jena.de>
2026-06-11 11:36:52 +0200

    add examples

--- src/routes/components/form/examples/MultiSelectExample.svelte
+++ src/routes/components/form/examples/MultiSelectExample.svelte
@@ -6,10 +6,37 @@
 	let target = ['Canada', 'Armenia'];
 	$: target;
 
+	let singletarget = 'Canada';
+	$: singletarget;
+
 	import { CountrieNames } from './exampleData';
+	import Select from 'svelte-select';
+
+
+	let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+
 </script>
 
 <div class="grid gap-10">
 	<MultiSelect id="multi_select_example" title="Required input fields" source={CountrieNames} bind:target />
 	<CodeBlock language="ts" code={JSON.stringify(target, undefined, 2)} />
 </div>
+
+<hr/>
+<div class="py-5">
+<Select
+
+		items={items}
+
+		label="water"
+		bind:value={singletarge}
+		clearFilterTextOnBlur={false}
+	/>
+
+	{singletarget["value"]} - {singletarget["label"]}
+
+	</div>
