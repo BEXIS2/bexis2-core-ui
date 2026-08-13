6a35cd0564d3b59dfdb8e68c1ebffd309c1e4610
david schoene <david.schoene@uni-jena.de>
2026-08-13 10:21:34 +0200

    set checkbox class

--- src/routes/components/form/examples/ToggleExample.svelte
+++ src/routes/components/form/examples/ToggleExample.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { SlideToggle } from '@skeletonlabs/skeleton';
+import Checkbox from '$lib/components/form/Checkbox.svelte';
+import { SlideToggle } from '@skeletonlabs/skeleton';
 
 	let value: boolean = false;
 	let label = 'toggle till the end of time';
 </script>
 
 <SlideToggle name={label} bind:checked={value} on:change>{label}</SlideToggle>
+
 <div class="pt-3"><b>Value:</b> {value}</div>
+
+<Checkbox	id="checkbox" bind:checked={value} on:change label={label} />
